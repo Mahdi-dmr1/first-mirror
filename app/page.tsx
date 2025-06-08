336f76b0cb77f708c4d09b2ce3f9391790f03f38
Mohammad <damirchi159@gmail.com>
2025-06-08 17:11:08 -0400

    Fix width size

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Page = () => {
 
     return(
         <div className="h-dvh w-full flex justify-center items-center">
-            <div className="w-1/3 bg-gray-200 flex flex-col gap-4 justify-center h-full items-center">
+            <div className="w-full bg-gray-200 flex flex-col gap-4 justify-center h-full items-center">
                 <p className="text-2xl text-black font-semibold">Hello World this is first mirror</p>
                 <button
                     className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
