4385b2188f8b1d5de00b93eef3489a51aca939e3
Mohammad <damirchi159@gmail.com>
2025-06-08 17:45:51 -0400

    Add button to send boolean message to parent window

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const Page = () => {
     // eslint-disable-next-line
     const [data, setData] = useState<any[]>([]);
+
+    const sendBooleanToParent = () => {
+        if (typeof window !== 'undefined' && window.parent) {
+            window.parent.postMessage(true, '*');
+        }
+    };
 
     useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
@@ -12,22 +18,27 @@ const Page = () => {
                 setData(event.data);
             }
         };
-
-        window.addEventListener("message", handleMessage);
-        return () => window.removeEventListener("message", handleMessage);
+        window.addEventListener('message', handleMessage);
+        return () => window.removeEventListener('message', handleMessage);
     }, []);
 
-    return (
+    return(
         <div className="h-dvh w-full flex justify-center items-center">
             <div className="w-full bg-gray-200 flex flex-col gap-4 justify-center h-full items-center">
                 <p className="text-2xl text-black font-semibold">Hello World this is first mirror</p>
+                <button
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+                    onClick={sendBooleanToParent}
+                >
+                    Open
+                </button>
                 <pre className="text-black bg-white p-4 rounded w-full max-w-xl overflow-x-auto">
                     {JSON.stringify(data, null, 2)}
                 </pre>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Page;
 
