9ad13dad96ea2fc22ba90e0577188813a7b4795c
Mohammad <damirchi159@gmail.com>
2025-06-08 16:35:51 -0400

    Add button

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,18 @@ const Page = () => {
 
     return(
         <div className="h-dvh w-full flex justify-center items-center">
-            <div className="w-1/3 bg-gray-200 flex justify-center h-full items-center">
+            <div className="w-1/3 bg-gray-200 flex flex-col gap-4 justify-center h-full items-center">
                 <p className="text-2xl text-black font-semibold">Hello World this is first mirror</p>
+                <button
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+                    onClick={() => window.parent && window.parent.postMessage('Button clicked from iframe', '*')}
+                >
+                    Send Message to Parent
+                </button>
             </div>
         </div>
     )
 }
 
 export default Page;
+
